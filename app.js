9a618866a71f4d5d1a1946ef37cedd246b417871
LingZZhu <lz407@cam.ac.uk>
2025-02-20 23:27:16 +0000

    update PLYLoader import to use the latest version for improved functionality

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import * as THREE from 'https://unpkg.com/three@0.150.0/build/three.module.js'; 
 import { OrbitControls } from 'https://unpkg.com/three@0.150.0/examples/jsm/controls/OrbitControls.js';
-import { PLYLoader } from 'https://unpkg.com/three@0.128.0/examples/jsm/loaders/PLYLoader.js';
+import { PLYLoader } from 'https://unpkg.com/three@0.150.0/examples/jsm/loaders/PLYLoader.js';
 
 let scene, camera, renderer;
 
