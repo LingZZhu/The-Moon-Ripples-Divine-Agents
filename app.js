4172fd50a9012dddfc44251719c787521d9670f0
LingZZhu <lz407@cam.ac.uk>
2025-02-20 20:28:46 +0000

    add initial setup for Three.js scene with GLTF model and orbit controls

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function init() {
 
   const loader = new GLTFLoader();
   loader.load(
-    './scene.gltf', // Replace with your model's path
+    './scene_september_bark.glb', // Replace with your model's path
     (gltf) => {
       scene.add(gltf.scene);
     },
